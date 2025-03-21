8a160bf12087c8940c28dce21e79b9b4fe92b01a
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-03-21 11:48:31 +0800

    Update gp-database-quickstart.sql

--- gp-database-quickstart.sql
+++ gp-database-quickstart.sql
@@ -12,6 +12,9 @@ insert into t1 SELECT generate_series(1,100);
 
 select * from pg_class where relname = 't1';
 
+SELECT attname, atttypid::regtype, attlen, attnotnull, atthasdef FROM pg_attribute 
+WHERE attrelid = (SELECT oid FROM pg_class WHERE relname = 't1') 
+AND attnum > 0;
 
 
 explain select * from t1 where id = 10;
